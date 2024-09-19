37a0619c115f1cbe222cc0323eb6509351c44dcf
shubham chauhan <shubhamsc9504@h=gmail.com>
2024-09-20 04:30:10 +0530

    text to json

--- users/views.py
+++ users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hello")
+    return HttpResponse("hello", content_type="application/json")
